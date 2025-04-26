75f9fd7250672959e510161492afb03bd571b4bd
Dhruv Narang <narang.dhruv123@gmail.com>
2025-04-26 17:23:22 +0530

    Added logging using Slf4j and custom log file

--- src/main/java/com/spring/journalApp/service/JournalEntryService.java
+++ src/main/java/com/spring/journalApp/service/JournalEntryService.java
@@ -3,7 +3,10 @@ package com.spring.journalApp.service;
 import com.spring.journalApp.Entity.JournalEntry;
 import com.spring.journalApp.Entity.User;
 import com.spring.journalApp.repository.JournalEntryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class JournalEntryService {
 
     @Autowired
@@ -23,11 +27,16 @@ public class JournalEntryService {
 
     @Transactional
     public void saveEntry(JournalEntry journalEntry,String username){
-        User user= userService.findByUser(username);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved= journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveUser(user);
+        try {
+            User user = userService.findByUser(username);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveUser(user);
+        } catch (Exception e) {
+            log.error("Hahahahaha");
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
--- src/main/java/com/spring/journalApp/service/UserService.java
+++ src/main/java/com/spring/journalApp/service/UserService.java
@@ -2,7 +2,10 @@ package com.spring.journalApp.service;
 
 import com.spring.journalApp.Entity.User;
 import com.spring.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -27,6 +31,11 @@ public class UserService {
                 userRepository.save(user);
                 return true;
             } catch (Exception e) {
+                log.error("hahahhahhahahahah");
+                log.warn("hahahhahhahahahah");
+                log.info("hahahhahhahahahah");
+                log.debug("hahahhahhahahahah");
+                log.trace("hahahhahhahahahah");
                 return false;
             }
     }
